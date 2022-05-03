88d4af54d3fdf5ca349d89198f672828355e4a63
Nella1a <m.allen@gmx.at>
2022-05-03 17:54:21 +0200

    Second version

--- inputOutput.py
+++ inputOutput.py
@@ -1,30 +1,34 @@
 start_form = "n"
-start_form = input("Please press 'y' to start the form: ")
-
-if start_form == "y":
+start_form = input("Please press 'y' to start the form input: ")
+users = []
+while start_form == "y":
+    start_form = "n"
     print("This is a short survey: ")
     user_first_name = input("first name:  ")
     user_last_name = input("last name:  ")
-    user_age = 0
+    user_gender = input("gender(m/w/x): ")
     try:
         user_age = int(input("Age: "))
     except ValueError:
-        while type(user_age) != int:
-            print("Error: Your input ist not a number")
-            user_age = int(input("Age(please enter a number): "))
+        print("Error: Your input ist not a number")
+        user_age = int(input("Age(please enter a number): "))
 
     user_jobtitle = input("Job Title: ")
-    user_hobbies = list(input("Your hobbies: "))
-    next_form_step = input("Press y to continue or n to exit")
-
-    if next_form_step == "y":
-        pass
-    else:
-        print("Your input: \n")
-        print(f"Your name is {user_first_name} {user_last_name}.")
-        print(f"You are {user_age} years old. Your job title is {user_jobtitle}")
-        print("Your hobbies are:")
-        for hobby in user_hobbies:
-            print(hobby)
-else:
-    print("No forminput provided")
+    user_hobbies = input("Your hobbies: ")
+    # user_list_hobbies = list(user_hobbies)
+    users.append({
+        "first_name": user_first_name,
+        "last_name": user_last_name,
+        "gender": user_gender,
+        "age": user_age,
+        "job_title": user_jobtitle,
+        "hobbies": user_hobbies
+    })
+    start_form = input("Press 'y' to add another user or 'n' to exit: ")
+    if start_form == "n":
+        for user in users:
+            print(f"User Index: {users.index(user)} \n")
+            print(f'full name: {user["first_name"]} {user["last_name"]}')
+            print(f'gender and age: {user["gender"]} , {user["age"]}')
+            print(f'Job title: {user["job_title"]}')
+            print(f'Hobbies: {user["hobbies"]}')
